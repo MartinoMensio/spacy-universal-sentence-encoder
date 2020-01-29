3eca1b261e363c589e533b24041482b80dbb27f1
Martino Mensio <martino.mensio@open.ac.uk>
2020-01-29 17:39:33 +0000

    version increase

--- build_use.sh
+++ build_use.sh
@@ -8,9 +8,9 @@ cp meta/meta.json use_model/meta.json
 # create the package
 mkdir -p use_package
 python -m spacy package use_model use_package --force
-pushd use_package/en_use-0.1.0
+pushd use_package/en_use-0.1.1
 # zip it
 python setup.py sdist
-# install the tar.gz from dist/en_use-0.1.0.tar.gz
-pip install dist/en_use-0.1.0.tar.gz
+# install the tar.gz from dist/en_use-0.1.1.tar.gz
+pip install dist/en_use-0.1.1.tar.gz
 popd

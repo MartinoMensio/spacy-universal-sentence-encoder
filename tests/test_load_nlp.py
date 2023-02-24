94d994ee4e51c2da98198debead9447b0009d82d
Martino Mensio <martino.mensio@open.ac.uk>
2023-02-24 13:40:30 +0100

    matrix workflow

--- tests/test_load_nlp.py
+++ tests/test_load_nlp.py
@@ -5,3 +5,14 @@ def test_basic_load():
     p = nlp.add_pipe('universal_sentence_encoder')
     assert p != None
     assert 'universal_sentence_encoder' in nlp.pipe_names
+
+def test_load_vector():
+    nlp = spacy.blank("en")
+    p = nlp.add_pipe('universal_sentence_encoder')
+    assert p != None
+    assert 'universal_sentence_encoder' in nlp.pipe_names
+    doc = nlp("This is a test")
+    vector = doc.vector
+    assert vector is not None
+    shape = vector.shape
+    assert shape != None

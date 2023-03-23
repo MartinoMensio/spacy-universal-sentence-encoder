03596442aa12379459906b8a4248b538739641a4
Martino Mensio <martino.mensio@open.ac.uk>
2023-03-23 17:53:05 +0100

    stricter tensorflow-text for python3.9

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from subprocess import getoutput
 
-with open('README.md', encoding='utf-8') as f:
+with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
+
 def setup_package():
-    setup(name="spacy_universal_sentence_encoder",
-    packages=find_packages(),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    package_data={'spacy_universal_sentence_encoder': ['meta/*.json']},
-    include_package_data=True
+    setup(
+        name="spacy_universal_sentence_encoder",
+        packages=find_packages(),
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        package_data={"spacy_universal_sentence_encoder": ["meta/*.json"]},
+        include_package_data=True,
     )
+
 
 if __name__ == "__main__":
     setup_package()
